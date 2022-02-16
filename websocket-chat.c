880d88fc145ebe2dd9672e0982534b3f2c05f2ce
Leonardo Araujo <leonardo.aa88@gmail.com>
2022-02-16 01:49:58 -0300

    Change to ipv6

--- websocket-chat.c
+++ websocket-chat.c
@@ -17,14 +17,14 @@ int server_socket; // global variable in order to be handled after SIGINT
 pthread_t t1, t2;
 pthread_mutex_t mutex;
 
-void handle_signal(int sig);                                                               // interrupt signal handler
-void check_socket(int socket);                                                             // checks socket
-void check_connection(int socket, struct sockaddr_in address);                             // checks connection
-void check_bind(int server_socket, struct sockaddr_in *server_address);                    // check binding
-void check_listen(int server_socket, int number_connections);                              // check connection listening
-void check_accept(int server_socket, int *client_socket, struct sockaddr *client_address); // check accepting connection
-void *send_message(void *socket);                                                          // sends message
-void *recv_message(void *socket);                                                          // receives message
+void handle_signal(int sig);                                                                   // interrupt signal handler
+void check_socket(int socket);                                                                 // checks socket
+void check_connection(int socket, struct sockaddr_in6 address);                                // checks connection
+void check_bind(int server_socket, struct sockaddr_in6 *server_address);                       // check binding
+void check_listen(int server_socket, int number_connections);                                  // check connection listening
+void check_accept(int server_socket, int *client_socket, struct sockaddr_in6 *client_address); // check accepting connection
+void *send_message(void *socket);                                                              // sends message
+void *recv_message(void *socket);                                                              // receives message
 
 int main(int argc, char *argv[])
 {
@@ -36,13 +36,14 @@ int main(int argc, char *argv[])
     printf("IP address: %s - port: %d\n", ip, port);
 
     puts("Creating socket ...");
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket = socket(AF_INET6, SOCK_STREAM, 0);
     check_socket(server_socket);
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;            // specifies protocol IPv4
-    server_address.sin_port = htons(port);          // specifies port (host to network)
-    server_address.sin_addr.s_addr = inet_addr(ip); // converts IP string to standard IPv4 decimal notation
+    struct sockaddr_in6 server_address;
+    server_address.sin6_family = AF_INET6;  // specifies protocol IPv6
+    server_address.sin6_port = htons(port); // specifies port (host to network)
+    server_address.sin6_addr = in6addr_any;
+    // inet_pton(AF_INET6, ip, &(server_address.sin6_addr));
 
     puts("Binding socket ...");
     check_bind(server_socket, &server_address);
@@ -52,10 +53,10 @@ int main(int argc, char *argv[])
     while (true)
     {
         int *client_socket = (int *)malloc(sizeof(int));
-        struct sockaddr_in client_address;
+        struct sockaddr_in6 client_address;
 
         check_listen(server_socket, 1);
-        check_accept(server_socket, (int *)client_socket, (struct sockaddr *)&client_address);
+        check_accept(server_socket, client_socket, (struct sockaddr_in6 *)&client_address);
         pthread_create(&t1, NULL, send_message, (void *)client_socket);
         pthread_create(&t2, NULL, recv_message, (void *)client_socket);
     }
@@ -95,9 +96,9 @@ void check_socket(int socket)
     puts("Socket created!");
 }
 
-void check_connection(int socket, struct sockaddr_in address)
+void check_connection(int socket, struct sockaddr_in6 address)
 {
-    if ((connect(socket, (struct sockaddr *)&address, sizeof(address))) < 0)
+    if ((connect(socket, (struct sockaddr_in6 *)&address, sizeof(address))) < 0)
     {
         perror("Connection error");
         printf("Error code: %d\n", errno);
@@ -106,7 +107,7 @@ void check_connection(int socket, struct sockaddr_in address)
     puts("Connected!");
 }
 
-void check_bind(int server_socket, struct sockaddr_in *server_address)
+void check_bind(int server_socket, struct sockaddr_in6 *server_address)
 {
     if ((bind(server_socket, (struct sockaddr *)server_address, sizeof(*server_address))) < 0)
     {
@@ -126,7 +127,7 @@ void check_listen(int server_socket, int num_connections)
     }
 }
 
-void check_accept(int server_socket, int *client_socket, struct sockaddr *client_address)
+void check_accept(int server_socket, int *client_socket, struct sockaddr_in6 *client_address)
 {
     if ((*client_socket = accept(server_socket, (struct sockaddr *)client_address, (socklen_t *)sizeof(client_address))) < 0)
     {
